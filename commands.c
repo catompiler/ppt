9f328f76786b76b47dc185966127032897e66b40
unknown <al.bauras@gmail.com>
2017-05-11 15:43:53 +0500

    In menu adds commands and access level

---
+++ commands.c
@@ -0,0 +1,66 @@
+#include "commands.h"
+#include "drive.h"
+#include "settings.h"
+#include "drive_events.h"
+#include <stdlib.h>
+
+/**
+ * Функция сравнения двух параметров по идентификатору.
+ * @param a Параметр А.
+ * @param b Параметр Б.
+ * @return Результат сравнения.
+ */
+static int commands_compare_ids(const void * key, const void * item)
+{
+    command_id_t item_id = ((command_menu_t*)item)->id;
+    command_id_t id = (command_id_t)(unsigned long)key;
+    
+    if(id > item_id) return 1;
+    if(id < item_id) return -1;
+    
+    return 0;
+}
+
+command_menu_t* command_get_by_id(command_id_t id) 
+{
+    command_menu_t* command = bsearch((void*)(unsigned long)id, menu_commands, COMMANDS_COUNT, sizeof(command_menu_t), commands_compare_ids);
+    return command;
+}
+
+bool command_start_stop(void) {
+    switch(drive_state()) {
+        case DRIVE_STATE_INIT: //!< Инициализация.
+        case DRIVE_STATE_CALIBRATION: //!< Калибровка питания.
+            break;
+        case DRIVE_STATE_IDLE: //!< Простой (готовность).
+        case DRIVE_STATE_STOP: //!< Останов.
+            return drive_start();
+        case DRIVE_STATE_START: //!< Запуск.
+        case DRIVE_STATE_RUN: //!< Работа.
+            return drive_stop();
+            break;
+        case DRIVE_STATE_STOP_ERROR: //!< Останов при ошибке.
+        case DRIVE_STATE_ERROR:  //!< Ошибка.
+            break;
+    }
+    return false;
+}
+
+bool command_enable_drive(void) {
+    // TODO
+    return false;
+}
+
+bool command_settings_save(void) {
+    return (settings_write() == E_NO_ERROR);
+}
+
+bool command_state_save(void) {
+    return (drive_events_write() == E_NO_ERROR);
+}
+
+bool command_settings_default(void) {
+    return (settings_default() == E_NO_ERROR);
+}
+
+
---
+++ commands.h
@@ -0,0 +1,57 @@
+/* 
+ * @file commands.h Команды меню.
+ */
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "commands_ids.h"
+
+//! Тип функции
+typedef bool (*commands_callback_t)();
+
+//! Тип идентификатора команды меню.
+typedef uint16_t command_id_t;
+
+//! Тип команды меню.
+typedef struct _Command_Menu {
+    command_id_t id;
+    commands_callback_t callback;
+} command_menu_t;
+
+//! Начинает список команд меню.
+#define MENU_COMMANDS(arg_name, arg_count)\
+        static const command_menu_t arg_name[arg_count] = 
+
+//! Описывает команду меню.
+#define MENU_COMMAND(arg_id, arg_function)\
+        { .id = arg_id, .callback = (commands_callback_t)arg_function }
+
+extern command_menu_t* command_get_by_id(command_id_t id);
+
+extern bool command_start_stop(void);
+
+extern bool command_enable_drive(void);
+
+extern bool command_settings_save(void);
+
+extern bool command_state_save(void);
+
+extern bool command_settings_default(void);
+
+// Количество команд меню.
+#define COMMANDS_COUNT 5
+
+// Объявление команд меню.
+MENU_COMMANDS(menu_commands, COMMANDS_COUNT) {
+    MENU_COMMAND(CMD_ID_START_STOP, &command_start_stop),
+    MENU_COMMAND(CMD_ID_ENABLE_DRIVE, &command_enable_drive),
+    MENU_COMMAND(CMD_ID_SETTINGS_SAVE, &command_settings_save),
+    MENU_COMMAND(CMD_ID_STATE_SAVE, &command_state_save),
+    MENU_COMMAND(CMD_ID_SETTINGS_DEFAULT, &command_settings_default),
+};
+
+#endif /* COMMANDS_H */
+
